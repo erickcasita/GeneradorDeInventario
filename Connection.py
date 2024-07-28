c054ed00cbd7ff1570ffc0744ca19efca148efb3
Erick Casanova <casa.erickmr@gmail.com>
2024-07-28 14:37:54 -0600

    add enviorement variables sql server

--- Connection.py
+++ Connection.py
@@ -1,8 +1,12 @@
 import pyodbc
-
+from dotenv import load_dotenv
+import os
+# Load environment variables from the .env file (if present)
+load_dotenv()
 def connection():
+    con = f'DRIVER={os.getenv('SQLSERVER_DIVER')};SERVER={os.getenv('SQLSERVER_HOST')};DATABASE={os.getenv('SQLSERVER_BD')};UID={os.getenv('SQLSERVER_USER')};PWD={os.getenv('SQLSERVER_PASSWORD')}'
     try:
-        connection = pyodbc.connect('DRIVER={SQL Server};SERVER=DBMSATUXTLA;DATABASE=BDSIVE;UID=comersat;PWD=Soporte%%2022')
+        connection = pyodbc.connect(con)
         return connection
     except Exception as ex:
         print("Error durante la conexión: {}".format(ex))
