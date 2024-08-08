250627a9fd4cd5ceb4f591d24700d3202be18a58
Erick Casanova <casa.erickmr@gmail.com>
2024-08-08 13:30:41 -0600

    Add validate date expiration and fix email spam

---
+++ Activate.py
@@ -0,0 +1,42 @@
+import requests,datetime
+
+def Get_current_date():
+    url = 'https://software-api-rnig.onrender.com/currentdate'
+    data = requests.get(url)
+    try:
+        if (data.status_code == 200):
+            data  = data.json()
+            return data['CurrentDate']
+          
+        else:
+            return "error";
+    except ValueError:
+        print("Error al obtener la fecha del servidor");
+        
+def Get_current_expiration_software():
+    url = 'https://software-api-rnig.onrender.com/currentversion'
+    data = requests.get(url)
+    try:
+        if (data.status_code == 200):
+            data  = data.json()
+            return data['Expiration']
+          
+        else:
+            return "error";
+    except ValueError:
+        print("Error al obtener la fecha del servidor");
+
+def Check_expiration_software(datecurrent, datesoftware):
+    
+    if (datecurrent !="error" and datesoftware != "error"):
+        dateFormatter = "%Y-%m-%d"
+        fecha_server=datetime.datetime.strptime(datecurrent, dateFormatter)
+        fecha_software = datetime.datetime.strptime(datesoftware, dateFormatter)
+        fecha_server = fecha_server.date();
+        fecha_software = fecha_software.date();
+        if(fecha_software>fecha_server):
+            return True
+        else:
+            return False
+    else:
+        return " \n ..... Error al obtener los datos de validación ..... || Contacte a su administrador"
--- helpers.py
+++ helpers.py
@@ -1,7 +1,12 @@
-import datetime, smtplib,time,os
+import datetime, smtplib,time,os, email
 from progress1bar import ProgressBar
+from email import encoders
 from email.message import EmailMessage
+from email.mime.base import MIMEBase
 from email.utils import formataddr
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 def validatedate(date_text):
         try:
             datetime.date.fromisoformat(date_text)
@@ -10,14 +15,365 @@ def validatedate(date_text):
           print ("\n Formato de fecha incorrecto, Formato:  YYYY-MM-DD")
           
 def getMessageContent():
-  with open('mails/MailTemplate.html') as fichero:
+  html = """ <!doctype html>
+<html lang="es">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Email Agencia Corona</title>
+    <style media="all" type="text/css">
+    /* -------------------------------------
+    GLOBAL RESETS
+------------------------------------- */
     
-    return fichero.read()
+    body {
+      font-family: Helvetica, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      font-size: 16px;
+      line-height: 1.3;
+      -ms-text-size-adjust: 100%;
+      -webkit-text-size-adjust: 100%;
+    }
+    
+    table {
+      border-collapse: separate;
+      mso-table-lspace: 0pt;
+      mso-table-rspace: 0pt;
+      width: 100%;
+    }
+    
+    table td {
+      font-family: Helvetica, sans-serif;
+      font-size: 16px;
+      vertical-align: top;
+    }
+    /* -------------------------------------
+    BODY & CONTAINER
+------------------------------------- */
+    
+    body {
+      background-color: #f4f5f6;
+      margin: 0;
+      padding: 0;
+    }
+    
+    .body {
+      background-color: #f4f5f6;
+      width: 100%;
+    }
+    
+    .container {
+      margin: 0 auto !important;
+      max-width: 600px;
+      padding: 0;
+      padding-top: 24px;
+      width: 600px;
+    }
+    
+    .content {
+      box-sizing: border-box;
+      display: block;
+      margin: 0 auto;
+      max-width: 600px;
+      padding: 0;
+    }
+    /* -------------------------------------
+    HEADER, FOOTER, MAIN
+------------------------------------- */
+    
+    .main {
+      background: #ffffff;
+      border: 1px solid #eaebed;
+      border-radius: 16px;
+      width: 100%;
+    }
+    
+    .wrapper {
+      box-sizing: border-box;
+      padding: 24px;
+    }
+    
+    .footer {
+      clear: both;
+      padding-top: 24px;
+      text-align: center;
+      width: 100%;
+    }
+    
+    .footer td,
+    .footer p,
+    .footer span,
+    .footer a {
+      color: #9a9ea6;
+      font-size: 16px;
+      text-align: center;
+    }
+    /* -------------------------------------
+    TYPOGRAPHY
+------------------------------------- */
+    
+    p {
+      font-family: Helvetica, sans-serif;
+      font-size: 16px;
+      font-weight: normal;
+      margin: 0;
+      margin-bottom: 16px;
+    }
+    
+    a {
+      color: #0867ec;
+      text-decoration: underline;
+    }
+    /* -------------------------------------
+    BUTTONS
+------------------------------------- */
+    
+    .btn {
+      box-sizing: border-box;
+      min-width: 100% !important;
+      width: 100%;
+    }
+    
+    .btn > tbody > tr > td {
+      padding-bottom: 16px;
+    }
+    
+    .btn table {
+      width: auto;
+    }
+    
+    .btn table td {
+      background-color: #ffffff;
+      border-radius: 4px;
+      text-align: center;
+    }
+    
+    .btn a {
+      background-color: #ffffff;
+      border: solid 2px #0867ec;
+      border-radius: 4px;
+      box-sizing: border-box;
+      color: #0867ec;
+      cursor: pointer;
+      display: inline-block;
+      font-size: 16px;
+      font-weight: bold;
+      margin: 0;
+      padding: 12px 24px;
+      text-decoration: none;
+      text-transform: capitalize;
+    }
+    
+    .btn-primary table td {
+      background-color: #0867ec;
+    }
+    
+    .btn-primary a {
+      background-color: #0867ec;
+      border-color: #0867ec;
+      color: #ffffff;
+    }
+    
+    @media all {
+      .btn-primary table td:hover {
+        background-color: #ec0867 !important;
+      }
+      .btn-primary a:hover {
+        background-color: #ec0867 !important;
+        border-color: #ec0867 !important;
+      }
+    }
+    
+    /* -------------------------------------
+    OTHER STYLES THAT MIGHT BE USEFUL
+------------------------------------- */
+    
+    .last {
+      margin-bottom: 0;
+    }
+    
+    .first {
+      margin-top: 0;
+    }
+    
+    .align-center {
+      text-align: center;
+    }
+    
+    .align-right {
+      text-align: right;
+    }
+    
+    .align-left {
+      text-align: left;
+    }
+    
+    .text-link {
+      color: #0867ec !important;
+      text-decoration: underline !important;
+    }
+    
+    .clear {
+      clear: both;
+    }
+    
+    .mt0 {
+      margin-top: 0;
+    }
+    
+    .mb0 {
+      margin-bottom: 0;
+    }
+    
+    .preheader {
+      color: transparent;
+      display: none;
+      height: 0;
+      max-height: 0;
+      max-width: 0;
+      opacity: 0;
+      overflow: hidden;
+      mso-hide: all;
+      visibility: hidden;
+      width: 0;
+    }
+    
+    .powered-by a {
+      text-decoration: none;
+    }
+    
+    /* -------------------------------------
+    RESPONSIVE AND MOBILE FRIENDLY STYLES
+------------------------------------- */
+    
+    @media only screen and (max-width: 640px) {
+      .main p,
+      .main td,
+      .main span {
+        font-size: 16px !important;
+      }
+      .wrapper {
+        padding: 8px !important;
+      }
+      .content {
+        padding: 0 !important;
+      }
+      .container {
+        padding: 0 !important;
+        padding-top: 8px !important;
+        width: 100% !important;
+      }
+      .main {
+        border-left-width: 0 !important;
+        border-radius: 0 !important;
+        border-right-width: 0 !important;
+      }
+      .btn table {
+        max-width: 100% !important;
+        width: 100% !important;
+      }
+      .btn a {
+        font-size: 16px !important;
+        max-width: 100% !important;
+        width: 100% !important;
+      }
+    }
+    /* -------------------------------------
+    PRESERVE THESE STYLES IN THE HEAD
+------------------------------------- */
+    
+    @media all {
+      .ExternalClass {
+        width: 100%;
+      }
+      .ExternalClass,
+      .ExternalClass p,
+      .ExternalClass span,
+      .ExternalClass font,
+      .ExternalClass td,
+      .ExternalClass div {
+        line-height: 100%;
+      }
+      .apple-link a {
+        color: inherit !important;
+        font-family: inherit !important;
+        font-size: inherit !important;
+        font-weight: inherit !important;
+        line-height: inherit !important;
+        text-decoration: none !important;
+      }
+      #MessageViewBody a {
+        color: inherit;
+        text-decoration: none;
+        font-size: inherit;
+        font-family: inherit;
+        font-weight: inherit;
+        line-height: inherit;
+      }
+    }
+    </style>
+  </head>
+  <body>
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
+      <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+          <div class="content">
+
+            <!-- START CENTERED WHITE CONTAINER -->
+            <span class="preheader">Inventario de almacenes al periodo {{dia}}.</span>
+            <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="main">
+
+              <!-- START MAIN CONTENT AREA -->
+              <tr>
+                <td class="wrapper">
+                  <img src="https://i.imgur.com/X2mQfjW.png" alt="Logo - Corona" width="300" height="260" border="0" style="border:0; outline:none; text-decoration:none; display:block; margin-left:auto ; margin-right: auto;">
+                  <div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:28px;font-weight:bold;line-height:1;text-align:center;color:#555;">
+                    Inventario de Almacenes al dia {{dia}}
+                  </div>
+                  <br>
+                  <p>¡Buen día!</p>
+                  <br>
+                  <p>Por medio de la presente, les hago llegar el inventario correspondiente al almacén de lleno San andrés Tuxlta y almacén de lleno Juan Díaz Covarrubias. </p>
+                  <br>
+                  <p>Saludos Cordiales</p>
+                </td>
+              </tr>
+              
+
+              <!-- END MAIN CONTENT AREA -->
+              </table>
+
+            <!-- START FOOTER -->
+            <div class="footer">
+              <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td class="content-block">
+                    <span class="apple-link">Comercializadora de Cervezas de los Tuxtlas S.A DE C.V. </span>
+                    <br> Encargado de almacén -- almacensat@coronalostuxtlas.com.mx
+                  </td>
+                </tr>
+                <tr>
+                  <td class="content-block powered-by">
+                    Desarrolado por  <a href="#">Ing. Erick M. Ramírez Casanova</a>
+                  </td>
+                </tr>
+              </table>
+            </div>
+
+            <!-- END FOOTER -->
+            
+<!-- END CENTERED WHITE CONTAINER --></div>
+        </td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+  </body>
+</html> """
+  return html 
 def getnameAttachemnt():
   with open('mails/attachment.name.mail') as fichero:
-    
-    return fichero.read() 
-  
+    return fichero.read()
 def getccmail():
   cc = [] 
   with open('mails/mails.em', 'r') as fichero:
@@ -31,24 +387,30 @@ def sendMail(date, remitente,destinatario,cc):
   pathadjunto = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Documents','ReporteadorInventario',adjuntoname)
   mensaje = getMessageContent()
   mensaje = mensaje.replace("{{dia}}", date)
-  email = EmailMessage()
-  email["From"] = formataddr(('Almacén SAT | Corona los Tuxtlas ', remitente))
-  email["To"] = destinatario
-  email["Cc"] = cc
-  email["Subject"] = "INVENTARIOS DE ALMACENES "+date + " 🍻"
-  email.set_content(mensaje, subtype="html")
-  with open(pathadjunto, "rb") as f:
-    email.add_attachment(
-        f.read(),
-        filename=adjuntoname,
-        maintype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        subtype="xlsx"
-    )
-  smtp = smtplib.SMTP_SSL("smtp.coronalostuxtlas.com.mx")
-  smtp.login(remitente, "Alm$sat&22")
-  smtp.sendmail(remitente,(destinatario + cc), email.as_string())
-  smtp.quit()
-  #Remove Attachment.name.file
+ 
+  html_part = MIMEText(mensaje, 'html','utf-8')
+  msg_alternative = MIMEMultipart('alternative')
+  msg_alternative.attach(html_part)
+
+
+  fp=open(pathadjunto,'rb')
+  attachment = MIMEApplication(fp.read(),_subtype="xlsx")
+  fp.close()
+  attachment.add_header('Content-Disposition', 'attachment', filename=adjuntoname)
+
+  msg_mixed = MIMEMultipart('mixed')
+  msg_mixed.attach(msg_alternative)
+  msg_mixed.attach(attachment)
+  msg_mixed['From'] =  formataddr(('Almacén SAT | Corona los Tuxtlas' , remitente))
+  msg_mixed['To'] = ', '.join(destinatario)
+  msg_mixed['Cc'] = ', '.join(cc)
+  msg_mixed['Subject'] =  "INVENTARIOS DE ALMACENES "+date + " 🍻"
+
+  smtp_obj = smtplib.SMTP_SSL('smtp.coronalostuxtlas.com.mx')
+  smtp_obj.ehlo()
+  smtp_obj.login(remitente, 'Alm$sat&22')
+  smtp_obj.sendmail(msg_mixed['From'], (destinatario + cc), msg_mixed.as_string())
+  smtp_obj.quit()
   os.remove('mails/attachment.name.mail')
 def progressbarmail():
   kwargs = {
@@ -64,3 +426,5 @@ def progressbarmail():
       for _ in range(pb.total):
           pb.count += 1
           time.sleep(0.3)
+
+
